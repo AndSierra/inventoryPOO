8a0abf37121b872327d1b14d8bd13d351f02a061
Paula Sierra <andreaxierra40@gmail.com>
2022-05-03 00:47:17 -0500

    Subiendo carpeta del proyecto

---
+++ src/modelo/Product.java
@@ -0,0 +1,46 @@
+
+package modelo;
+
+public class Product {
+    private int keyPdt;
+    private String nombrePdt;
+    private int precioPdt;
+    private int numUnidadesPdt;
+
+//    getters and setters
+    
+    public int getKeyPdt() {
+        return keyPdt;
+    }
+
+    public void setKeyPdt(int keyPdt) {
+        this.keyPdt = keyPdt;
+    }
+
+    public String getNombrePdt() {
+        return nombrePdt;
+    }
+
+    public void setNombrePdt(String nombrePdt) {
+        this.nombrePdt = nombrePdt;
+    }
+
+    public int getPrecioPdt() {
+        return precioPdt;
+    }
+
+    public void setPrecioPdt(int precioPdt) {
+        this.precioPdt = precioPdt;
+    }
+
+    public int getNumUnidadesPdt() {
+        return numUnidadesPdt;
+    }
+
+    public void setNumUnidadesPdt(int numUnidadesPdt) {
+        this.numUnidadesPdt = numUnidadesPdt;
+    }
+    
+    
+    
+}
---
+++ src/modelo/connection.java
@@ -0,0 +1,29 @@
+
+package modelo;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+
+public class connection {
+    
+    public static Connection getconnection(){
+        
+        String url = "jdbc:sqlserver://localhost:1433;"
+                + "database = master;"
+                + "user = and;"
+                + "password = 12345;";
+        
+        try{
+            Connection con = DriverManager.getConnection(url);
+            return con;
+        }catch(SQLException e){
+            System.out.println(e.toString());
+            return null;
+        }
+    }
+
+}
+
+    
+    
